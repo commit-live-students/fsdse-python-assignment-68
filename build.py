706738d5b5e479d8ea82d37006d09661042407ea
gyandhanee <gyandhanee@users.noreply.github.com>
2017-06-02 07:13:23 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-
 def solution(array1, array2):
-    """
-    Enter your code here
-    """
+    array1 = np.array(array1)
+    array2 = np.array(array2)
+    a, b = array2.shape
+    array2 = np.reshape(array2, (a*b))
+    return np.hstack((array1,array2))
