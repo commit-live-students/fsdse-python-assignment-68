a31b245ce5f36fe0afd3772669f12052393cdd05
coded9 <coded9@users.noreply.github.com>
2017-06-11 15:00:10 +0000

    Done

--- build.py
+++ build.py
@@ -5,3 +5,6 @@ def solution(array1, array2):
     """
     Enter your code here
     """
+    return np.append(array1,array2)
+
+#https://docs.scipy.org/doc/numpy/reference/generated/numpy.append.html
