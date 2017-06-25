d7b31de9b88a6e76db1255faad43449a64d60570
birajparikh16 <birajparikh16@gmail.com>
2017-06-25 19:33:06 +0000

    Done

--- build.py
+++ build.py
@@ -1,7 +1,8 @@
 import numpy as np
 
-
 def solution(array1, array2):
-    """
-    Enter your code here
-    """
+    a = np.append(array1,array2)
+    return a
+
+# solution([10, 20, 30], [[40, 50, 60], [70, 80, 90]])
+# Output : array([10, 20, 30, 40, 50, 60, 70, 80, 90])
